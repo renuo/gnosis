ad4392e1092625b44fd05c5c8b18164c21082440
anes <75726773+aneshodza@users.noreply.github.com>
2023-06-01 15:22:10 +0200

    Update init.rb (#17)

--- init.rb
+++ init.rb
@@ -16,7 +16,7 @@ Redmine::Plugin.register :gnosis do
   name 'Gnosis plugin'
   author 'Anes Hodza'
   description 'This Plugin allows you to see the status of issues in a project'
-  version '0.0.1'
+  version '1.0.0'
   url 'https://github.com/aneshodza/gnosis/'
   author_url 'https://www.aneshodza.ch/'
 end
