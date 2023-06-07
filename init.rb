f2153d6ee25f62f0acc61cf4fd3cfa1e8c4a2923
Anes Hodza <anes.hodza@renuo.ch>
2023-06-07 13:49:50 +0200

    Fix yaml example

--- init.rb
+++ init.rb
@@ -4,13 +4,12 @@ require 'redmine'
 require_relative 'lib/issue_details_hook_listener'
 
 unless Rails.env.test?
-  yaml_data = nil
-  if File.exist?(Rails.root.join('plugins/gnosis/config/application.yml'))
-    yaml_data = YAML.safe_load(ERB.new(Rails.root.join('plugins/gnosis/config/application.yml').read).result)
-  else
-    Rails.logger.warn 'application.yml not found'
-    yaml_data = YAML.safe_load(ERB.new(Rails.root.join('plugins/gnosis/config/application.yml.example').read).result)
-  end
+  yaml_data = if File.exist?(Rails.root.join('plugins/gnosis/config/application.yml'))
+              YAML.safe_load(ERB.new(Rails.root.join('plugins/gnosis/config/application.yml').read).result)
+            else
+              Rails.logger.warn 'application.yml not found'
+              YAML.safe_load(ERB.new(Rails.root.join('plugins/gnosis/config/application.example.yml').read).result)
+            end
   ENV.merge!(ActiveSupport::HashWithIndifferentAccess.new(yaml_data))
 end
 
