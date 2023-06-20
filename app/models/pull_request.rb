48ade1d1d9a6296721092ab8c46eec3ae15b4a81
Anes Hodza <anes.hodza@renuo.ch>
2023-06-20 17:20:32 +0200

    DRY up pr stuff

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -5,17 +5,18 @@ class PullRequest < GnosisApplicationRecord
   has_many :pull_request_deployments, dependent: :destroy
 
   def self.auto_create_or_update(params)
-    pr = PullRequest.find_or_initialize_by(url: params[:pull_request][:html_url])
-    pr.update!(state: state(params), url: params[:pull_request][:html_url],
-               title: params[:pull_request][:title], source_branch: params[:pull_request][:head][:ref],
-               target_branch: params[:pull_request][:base][:ref], was_merged: params[:pull_request][:merged],
-               merge_commit_sha: params[:pull_request][:merge_commit_sha], issue_id: params[:issue_id])
+    pr = params[:pull_request]
+    pull_request = PullRequest.find_or_initialize_by(url: pr[:html_url])
+    pull_request.update!(state: state(params, pr), url: pr[:html_url],
+               title: pr[:title], source_branch: pr[:head][:ref],
+               target_branch: pr[:base][:ref], was_merged: pr[:merged],
+               merge_commit_sha: pr[:merge_commit_sha], issue_id: params[:issue_id])
   end
 
-  def self.state(params)
-    return 'merged' if params[:pull_request][:merged]
-    return 'draft' if params[:pull_request][:draft]
+  def self.state(params, pr)
+    return 'merged' if pr[:merged]
+    return 'draft' if pr[:draft]
 
-    params[:pull_request][:state]
+    pr[:state]
   end
 end
