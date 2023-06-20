173e2cb9ed80d51716ffb27498f42367f3b71a62
Anes Hodza <anes.hodza@renuo.ch>
2023-06-20 17:29:46 +0200

    Fix style

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -7,16 +7,16 @@ class PullRequest < GnosisApplicationRecord
   def self.auto_create_or_update(params)
     pr = params[:pull_request]
     pull_request = PullRequest.find_or_initialize_by(url: pr[:html_url])
-    pull_request.update!(state: state(params, pr), url: pr[:html_url],
-               title: pr[:title], source_branch: pr[:head][:ref],
-               target_branch: pr[:base][:ref], was_merged: pr[:merged],
-               merge_commit_sha: pr[:merge_commit_sha], issue_id: params[:issue_id])
+    pull_request.update!(state: state(pr), url: pr[:html_url],
+                         title: pr[:title], source_branch: pr[:head][:ref],
+                         target_branch: pr[:base][:ref], was_merged: pr[:merged],
+                         merge_commit_sha: pr[:merge_commit_sha], issue_id: params[:issue_id])
   end
 
-  def self.state(params, pr)
-    return 'merged' if pr[:merged]
-    return 'draft' if pr[:draft]
+  def self.state(pr_param)
+    return 'merged' if pr_param[:merged]
+    return 'draft' if pr_param[:draft]
 
-    pr[:state]
+    pr_param[:state]
   end
 end
