6aff443bd7d90ab4903ba2937c1f4509a1955572
Daniel Bengl <daniel.bengl@renuo.ch>
2024-01-12 17:08:54 +0100

    Update test file

--- app/services/pull_request_sync_service.rb
+++ app/services/pull_request_sync_service.rb
@@ -6,14 +6,9 @@ class PullRequestSyncService
 
     fetch_repositories.each do |repository|
       fetch_repository_pull_requests(repository).each do |pull_request|
-        ticket_number = NumberExtractor.call(pull_request: pull_request)
-
-        continue if ticket_number.blank?
-
-        Rails.logger.info "Processing pull request #{pull_request[:html_url]} for ticket #{ticket_number}"
+        Rails.logger.info "Processing pull request #{pull_request[:html_url]}"
 
         pull_request_hash = pull_request.to_h
-        pull_request_hash[:issue_id] = ticket_number
         pull_request_hash[:merged] = pull_request[:merged_at].present?
 
         WebhookHandler.handle_github(pull_request_hash)
@@ -24,7 +19,7 @@ class PullRequestSyncService
   end
 
   def fetch_repositories
-    @client.org_repositories(ENV.fetch('GITHUB_ORGANIZATION_NAME'), type: :all)
+    @client.org_repositories(ENV['GITHUB_ORGANIZATION_NAME'], type: :all)
   end
 
   def fetch_repository_pull_requests(repository)
@@ -32,6 +27,6 @@ class PullRequestSyncService
   end
 
   def init_client
-    @client = Octokit::Client.new(access_token: ENV.fetch('GITHUB_ACCESS_TOKEN'))
+    @client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
   end
 end
--- test/unit/pull_request_sync_service_test.rb
+++ test/unit/pull_request_sync_service_test.rb
@@ -1,13 +1,42 @@
 # frozen_string_literal: true
 
 require_relative '../test_helper'
-class PullRequestSyncServiceTest < Minitest::Test
-  def test
-    allow_any_instance_of(PullRequestSyncService).to receive(:fetch_repositories).and_return([{name: 'renuo/legacy-import-test'}])
-    expect(PullRequest.count).to eq(0)
+require 'minitest/mock'
 
-    PullRequestSyncService.new.call
+class PullRequestSyncServiceTest < ActiveSupport::TestCase
+  def setup
+    @github_repositories = [
+      { full_name: 'aneshodza/test-repo' },
+    ]
 
-    expect(PullRequest.count).to eq(1)
+    @github_pull_requests = [
+       {
+        state: 'closed',
+        html_url: 'https://github.com/aneshodza/test-repo/pull/17',
+        title: 'Create something',
+        head: {
+          ref: 'feature/1-some-feature'
+        },
+        base: {
+          ref: 'main'
+        },
+        merged_at: '2021-03-03T12:00:00Z',
+        merge_commit_sha: '19a89f0050eacf201ccd058d5e28cddf2b035bfc',
+        draft: false
+      }
+    ]
+
+    Octokit::Client.any_instance.stubs(:new).returns(@github_repositories)
+    Octokit::Client.any_instance.stubs(:org_repositories).returns(@github_repositories)
+    Octokit::Client.any_instance.stubs(:pull_requests).returns(@github_pull_requests)
+  end
+
+  def test_import_pull_requests
+    instance = PullRequestSyncService.new
+    instance.stub(:fetch_repositories, [{ name: 'renuo/legacy-import-test' }]) do
+      assert_equal 0, PullRequest.count
+      instance.call
+      assert_equal 1, PullRequest.count
+    end
   end
 end
