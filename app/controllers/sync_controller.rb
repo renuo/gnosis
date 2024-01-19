35e04a0f79e6a1272abf78a019271567163ef5a5
Daniel Bengl <daniel.bengl@renuo.ch>
2024-01-19 15:42:49 +0100

    Attempt fix linter

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -3,7 +3,7 @@
 class SyncController < ApplicationController
   def sync_pull_requests
     SyncJob.perform_later
-    render plain: "Pull request syncing started. This may take a while."
+    render plain: 'Pull request syncing started. This may take a while.'
   rescue StandardError => e
     Rails.logger.error(e)
     render plain: 'There was an error while syncing the pull requests, please check the logs.'
