afca96b8d8909c453798ac8b7c5002a311b7984c
Daniel Bengl <daniel.bengl@renuo.ch>
2024-01-19 14:40:01 +0100

    Fix protected controller

--- app/controllers/protected_controller.rb
+++ app/controllers/protected_controller.rb
@@ -2,18 +2,19 @@
 
 class ProtectedController < ApplicationController
   before_action :set_current_user
-  before_action :set_current_timer_session
   before_action :permission_manager
   before_action :verify_permission!
   skip_before_action :verify_authenticity_token
-
-  helper_method :offset_for_time_zone
 
   def verify_permission!
     return unless User.current
     return if permission_manager.can?(action_name.to_sym, controller_name.to_sym)
 
     render_403(flash: { error: t('timer_sessions.messages.errors.permission.no_access') })
+  end
+
+  def permission_manager
+    @permission_manager ||= PermissionManager.new
   end
 
   def set_current_user
