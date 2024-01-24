03561eae2eee458459a11c83ebe069efbe027408
Daniel Bengl <daniel.bengl@renuo.ch>
2024-01-24 15:44:00 +0100

    Remove secret defaults, refactor WebhookHandler

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -4,14 +4,16 @@ class PullRequest < GnosisApplicationRecord
   belongs_to :issue
   has_many :pull_request_deployments, dependent: :destroy
 
-  def self.auto_create_or_update(params)
-    state = params[:pull_request][:merged] ? 'merged' : params[:pull_request][:state]
-    state = 'draft' if params[:pull_request][:draft]
+  def self.auto_create_or_update(webhook_params)
+    state = webhook_params[:pull_request][:merged] ? 'merged' : webhook_params[:pull_request][:state]
+    state = 'draft' if webhook_params[:pull_request][:draft]
 
-    pr = PullRequest.find_or_initialize_by(url: params[:pull_request][:html_url])
-    pr.update!(state: state, url: params[:pull_request][:html_url],
-               title: params[:pull_request][:title], source_branch: params[:pull_request][:head][:ref],
-               target_branch: params[:pull_request][:base][:ref], was_merged: params[:pull_request][:merged],
-               merge_commit_sha: params[:pull_request][:merge_commit_sha], issue_id: params[:issue_id])
+    return if Issue.find_by(id: webhook_params[:issue_id]).nil?
+
+    pr = PullRequest.find_or_initialize_by(url: webhook_params[:pull_request][:html_url])
+    pr.update!(state: state, url: webhook_params[:pull_request][:html_url],
+               title: webhook_params[:pull_request][:title], source_branch: webhook_params[:pull_request][:head][:ref],
+               target_branch: webhook_params[:pull_request][:base][:ref], was_merged: webhook_params[:pull_request][:merged],
+               merge_commit_sha: webhook_params[:pull_request][:merge_commit_sha], issue_id: webhook_params[:issue_id])
   end
 end
