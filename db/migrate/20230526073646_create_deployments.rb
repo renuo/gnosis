e97fd8421654666924a8cb0568fec3e011f49ffc
Daniel Bengl <daniel.bengl@renuo.ch>
2024-03-26 16:20:09 +0100

    Update migrations

--- db/migrate/20230526073646_create_deployments.rb
+++ db/migrate/20230526073646_create_deployments.rb
@@ -4,7 +4,7 @@ class CreateDeployments < ActiveRecord::Migration[6.1]
       t.string :deploy_branch
       t.string :url
       t.boolean :has_passed, default: false, null: false
-      t.references :pull_request, null: false, foreign_key: true
+      t.references :pull_request, null: false
       t.datetime :ci_date
 
       t.timestamps
