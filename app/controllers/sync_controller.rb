4406783980617e6daaa2d4a998de6ec9579d88e9
Daniel Bengl <daniel.bengl@renuo.ch>
2024-04-09 15:41:15 +0200

    Update sync controller

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -4,10 +4,11 @@ class SyncController < ApplicationController
   before_action :require_admin
 
   def sync_pull_requests
-    SyncJob.perform_later
+    SyncJob.perform_now
     render plain: 'Pull request syncing started. This may take a while.'
   rescue StandardError => e
     Rails.logger.error(e)
-    render plain: 'There was an error while syncing the pull requests, please check the logs.'
+    render plain: 'There was an error while syncing the pull requests, please check the logs.',
+           status: :internal_server_error
   end
 end
