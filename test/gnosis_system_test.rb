20b90ce0cc47629134e957ee506d8aea558a27fb
Daniel Bengl <daniel.bengl@renuo.ch>
2025-02-25 11:46:15 +0100

    Fix tests

--- test/gnosis_system_test.rb
+++ test/gnosis_system_test.rb
@@ -5,7 +5,9 @@ require File.expand_path("#{File.dirname(__FILE__)}/../../../test/application_sy
 
 class GnosisSystemTest < ApplicationSystemTestCase
   def login
-    User.where(login: 'admin').update!(password: '12345678')
+    User.where(login: 'admin').each do |user|
+      user.update!(password: '12345678')
+    end
 
     visit '/login'
     fill_in 'username', with: 'admin'
